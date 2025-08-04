ae5d67d15ad2cfd2163664adbb207f5c1951cae0
rogerg6 <rogerg6@163.com>
2025-08-04 22:51:35 +0800

    [rtt] 解决动态无法创建thread的问题

--- rtthread-nano/bsp/stm32f103/rtconfig.h
+++ rtthread-nano/bsp/stm32f103/rtconfig.h
@@ -34,7 +34,7 @@
 // <h>Debug Configuration
 // <c1>enable kernel debug configuration
 //  <i>Default: enable kernel debug configuration
-//#define RT_DEBUG
+#define RT_DEBUG
 // </c>
 // <o>enable components initialization debug configuration<0-1>
 //  <i>Default: 0
@@ -104,8 +104,9 @@
 // </c>
 // <c1>Dynamic Heap Management(Algorithm: small memory )
 //  <i>Dynamic Heap Management
-// #define RT_USING_HEAP
-// #define RT_USING_SMALL_MEM
+#define RT_USING_HEAP
+#define RT_USING_SMALL_MEM
+#define RT_USING_SMALL_MEM_AS_HEAP
 // </c>
 // <c1>using tiny size of memory
 //  <i>using tiny size of memory
