d9e89ccc10cd6f693a9d61a8ecdc6eed342e6ce4
rogerg6 <rogerg6@163.com>
2025-08-04 23:01:46 +0800

    [rtt] 动态创建用户线程

--- rtthread-nano/bsp/stm32f103/main.c
+++ rtthread-nano/bsp/stm32f103/main.c
@@ -1,5 +1,24 @@
+#include "rtthread.h"
 #include "led.h"
 
-int main() {
+static rt_thread_t led_thrd = RT_NULL;
+
+static void led_thread_entry(void *param) {
     Led_Blink();
+}
+
+int main() {
+     led_thrd =                          /* 线程控制块指针 */
+    rt_thread_create( "led1",              /* 线程名字 */
+                    led_thread_entry,   /* 线程入口函数 */
+                    RT_NULL,             /* 线程入口函数参数 */
+                    512,                 /* 线程栈大小 */
+                    3,                   /* 线程的优先级 */
+                    20);                 /* 线程时间片 */
+
+    /* 启动线程，开启调度 */
+    if (led_thrd != RT_NULL)
+        rt_thread_startup(led_thrd);
+    else
+        return -1;
 }
